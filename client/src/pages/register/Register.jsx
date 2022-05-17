27dbd48f1b0fc3cd70bc577f87fbd66d246b703b
Fatime Nur <fatimenurc@gmail.com>
2022-05-17 11:33:04 +0300

    register part edited (upload photo)

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -24,31 +24,7 @@ export default function Register() {
               <input className="gender" type="radio" name="male" value="male"/><span className="color">Erkek</span>
               <input className="gender" type="radio" name="female" value="female"/><span className="color">Kadın</span>
             </span>
-            {/* <div className="profile-pic-div">
-              <img src={avatar} id="photo" alt=""/> 
-              <input id="file" type="file" /> 
-              <label id="uploadBtn">Choose Photo</label>
-            </div> */}
-           
-              {/* <div className="profile-pic-div">
-                <img src={avatar} id="photo" alt="" /> */}
-                <input type="file" className=" selectImg loginInput " accept="image/jpg, image/jpeg, image/png"/>
-              {/* </div> */}
-          
-       
-
-
-            {/* <input type="file" id="file">Choose Photo</input> */}
-{/* 
-
-            <h1>Upload Photo Like Facebook Profile DP</h1>
-
-<div class="profile-pic-div">
-  <img src="image.jpg" id="photo">
-  <input type="file" id="file">
-  <label for="file" id="uploadBtn">Choose Photo</label>
-</div> */}
-
+            <input type="file" className=" selectImg loginInput " accept="image/jpg, image/jpeg, image/png"/>
             <button className="loginButton">Kayıt ol</button>
             <button className="loginRegister">Hesabına giriş yap</button>
           </div>
