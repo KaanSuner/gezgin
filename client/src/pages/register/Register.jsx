2c350e457e98258fe073314dc3c3471f94b0e47e
Fatime Nur <fatimenurc@gmail.com>
2022-05-17 11:30:12 +0300

    register part edited (upload photo)

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,4 +1,5 @@
 import "./register.css";
+import avatar from "./image.jpg";
 
 export default function Register() {
   return (
@@ -19,10 +20,35 @@ export default function Register() {
             <input placeholder="Email" className="loginInput"></input>
             <input placeholder="Parola" className="loginInput"></input>
             <input placeholder="Telefon" className="loginInput"></input>
-            <span className="loginInput cinsiyet">
+            <span className="loginInput">
               <input className="gender" type="radio" name="male" value="male"/><span className="color">Erkek</span>
               <input className="gender" type="radio" name="female" value="female"/><span className="color">Kadın</span>
             </span>
+            {/* <div className="profile-pic-div">
+              <img src={avatar} id="photo" alt=""/> 
+              <input id="file" type="file" /> 
+              <label id="uploadBtn">Choose Photo</label>
+            </div> */}
+           
+              {/* <div className="profile-pic-div">
+                <img src={avatar} id="photo" alt="" /> */}
+                <input type="file" className=" selectImg loginInput " accept="image/jpg, image/jpeg, image/png"/>
+              {/* </div> */}
+          
+       
+
+
+            {/* <input type="file" id="file">Choose Photo</input> */}
+{/* 
+
+            <h1>Upload Photo Like Facebook Profile DP</h1>
+
+<div class="profile-pic-div">
+  <img src="image.jpg" id="photo">
+  <input type="file" id="file">
+  <label for="file" id="uploadBtn">Choose Photo</label>
+</div> */}
+
             <button className="loginButton">Kayıt ol</button>
             <button className="loginRegister">Hesabına giriş yap</button>
           </div>
