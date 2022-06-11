7a78e251398c813cb7d0b80435aa9916c3805955
Kaan <80221763+KaanSuner@users.noreply.github.com>
2022-06-11 03:00:32 +0300

    fix  travel or Acc

--- client/src/pages/travelOrAcc/TravelOrAcc.jsx
+++ client/src/pages/travelOrAcc/TravelOrAcc.jsx
@@ -4,37 +4,37 @@ import Hero from "../../components/Hero/Hero";
 import More from "../../components/more/more";
 import Footer from "../../components/footer/footer";
 import React from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import home from "./home.png";
 import car from "./car.png";
 
 export default function Login() {
+  const navigate = useNavigate();
+  const handleClick1 = () => navigate("/accomodation");
+  const handleClick2 = () => navigate("/travel");
+
   return (
     <>
     <Navbar type="travelOrAcc"/>
     <div className="login">
       <div className="loginWrapper">
         <div className="loginLeft">
-            <Link to={"../accomodation"}>
-            <div className="card OptionCard">
+            <div className="card OptionCard" onClick={handleClick1}>
             <img src={home} className="OptionImage" alt="" />
             <div className="card-body">
-                <h5 className="card-title option-text">Konaklama Bul</h5>
-                <p className="card-text option-text">Kriterlerine göre en uygun konaklama teklifini bul</p>
+                <h5 className="card-title option-text">Konaklama</h5>
             </div>
             </div>
-            </Link>
+           
         </div>
         <div className="loginRight">
-        <Link to={"../travel"}>
-        <div className="card OptionCard">
+        <div className="card OptionCard" onClick={handleClick2}>
             <img src={car} className="OptionImage" alt="" />
             <div className="card-body">
-                <h5 className="card-title option-text">Seyahat Bul</h5>
-                <p className="card-text option-text ">Kriterlerine göre en uygun seyahat teklifini bul</p>
+                <h5 className="card-title option-text">Seyahat</h5>
             </div>
             </div>
-            </Link>
+           
         </div>
       </div>
     </div>
